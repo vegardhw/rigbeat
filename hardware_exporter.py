58e60280b98ced3938201d26cd99bfacfa462a2d
Vegard Hoff Walmsness <vegardhw@gmail.com>
2025-11-18 15:24:21 +0100

    Add modules

--- hardware_exporter.py
+++ hardware_exporter.py
@@ -1,1 +1,11 @@
-# hardware_exporter.py
+"""
+Rigbeat - Prometheus Exporter
+Exports Windows hardware metrics (CPU/GPU temps, fan speeds, loads) for Prometheus/Grafana
+
+Requirements:
+    - LibreHardwareMonitor running with WMI enabled
+    - Python 3.8+
+    - pip install prometheus-client wmi pywin32
+"""
+import time
+import wmi
